9fbcc2639e0e50b29193c130eaa1577abcd0d2db
Scott Jones <scottdj92@gmail.com>
2019-03-16 17:12:53 -0400

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">Hello {{bundler}}</div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data: {
+        bundler: "Parcel"
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        color: cornflowerblue;
+    }
+</style>
+
---
+++ vue-shim.d.ts
@@ -0,0 +1,6 @@
+// https://github.com/vuejs/vue-cli/issues/1198
+
+declare module "*.vue" {
+    import Vue from "vue";
+    export default Vue;
+}
