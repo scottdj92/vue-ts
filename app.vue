e22f5223f35fd0de9c0dbb839ea879b753f6d312
Scott Jones <scottdj92@gmail.com>
2019-06-21 22:41:44 -0400

    update and test out prop passing in vue

--- app.vue
+++ app.vue
@@ -2,12 +2,14 @@
     <div class="container">
         <main-header></main-header>
         Hello {{bundler}}
+        <body-paragraph content="long form content"></body-paragraph>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Header from "./src/main-header.vue";
+import Paragraph from "./src/paragraph.vue";
 export default Vue.extend({
     data: () => {
         return {
---
+++ src/paragraph.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p class="body">{{ content }}</p>
+        <h4>longer form template</h4>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.component("body-paragraph", {
+    props: ["content"]
+});
+</script>
+
+<style scoped>
+    .body {
+        color: rebeccapurple;
+    }
+</style>
